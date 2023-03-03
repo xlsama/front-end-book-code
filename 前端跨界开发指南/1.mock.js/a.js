e1592e68a582869090b09a3f42ca828a6a762688
xlsama <im.xlsama@gmail.com>
2023-03-03 18:54:59 +0800

    chore: update

---
+++ 前端跨界开发指南/1.mock.js/a.js
@@ -0,0 +1,8 @@
+console.log(1);
+
+export default {
+  a: 'a',
+  b: 'b'
+};
+
+console.log(2);
---
+++ 前端跨界开发指南/1.mock.js/main.js
@@ -0,0 +1,20 @@
+// import Mock from 'mockjs';
+console.log(0);
+import q from './a.js';
+console.log(q.a);
+console.log(3);
+
+// const data = Mock.mock({
+//   // 属性 list 的值是一个数组，其中含有 1 到 10 个元素
+//   'list|1-10': [
+//     {
+//       // 属性 id 是一个自增数，起始值为 1，每次增 1
+//       'id|+1': 5,
+//       createTime: '@datetime'
+//     }
+//   ]
+// });
+
+// // 输出结果
+// console.log(data);
+// console.log(JSON.stringify(data, null, 4));
